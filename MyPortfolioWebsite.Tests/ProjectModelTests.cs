4b30dc4943cef0d2d410adebc39f291165630f90
Jacob Kyle Farrugia <jacobkylef@gmail.com>
2026-06-21 01:07:55 +0200

    Add automates tests to CI pipeline

---
+++ MyPortfolioWebsite.Tests/ProjectModelTests.cs
@@ -0,0 +1,26 @@
+﻿using MyPortfolioWebsite.Models;
+using Xunit;
+
+namespace MyPortfolioWebsite.Tests
+{
+    public class ProjectModelTests
+    {
+        [Fact]
+        public void Project_DefaultDates_AreSet()
+        {
+            var project = new Project();
+
+            Assert.True(project.CreatedAt <= DateTime.UtcNow);
+            Assert.Null(project.UpdatedAt);
+        }
+
+        [Fact]
+        public void AppUser_Projects_DefaultsToEmptyList()
+        {
+            var user = new AppUser();
+
+            Assert.NotNull(user.Projects);
+            Assert.Empty(user.Projects);
+        }
+    }
+}
